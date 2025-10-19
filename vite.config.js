f0252e796867ee17e7d03a8858963b7b443956c6
maxholub02 <122486753+maxgol29@users.noreply.github.com>
2025-10-19 18:16:43 -0500

    Set up frontend for prod

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 5173
-  },
+  base: '/e-commerce-react-frontend/'
 })
-
-
